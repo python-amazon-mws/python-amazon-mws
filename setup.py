6a0f14eaff5c18ecb64a731f33504c311770abd6
paulo <commonzenpython@gmail.com>
2012-02-13 06:38:21 -0500

    Added license, setup and main API file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="python-amazon-mws",
+    version="0.0.1",
+    description="A python interface for Amazon MWS",
+    author="Paulo Alvarado",
+    author_email="commonzenpython@gmail.com",
+    url="http://github.com/czpython/python-amazon-mws",
+    packages=['mws'],
+    
+)
+ 
