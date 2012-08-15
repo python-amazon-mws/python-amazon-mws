e4a9678df3c019c3a8e1d975dd6830db5eaaf8ba
bloodywing <bloodywing@neocomy.net>
2012-08-15 14:39:42 +0200

    Fixes big Memcached Objects

--- mws/utils.py
+++ mws/utils.py
@@ -40,7 +40,8 @@ class object_dict(dict):
             return d
 
         # if value is the only key in object, you can omit it
-
+    def __setstate__(self, item):
+        return False
     def __setattr__(self, item, value):
         self.__setitem__(item, value)
 
