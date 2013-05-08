21b1f9c876e3581f8106b6bef9899fd9275ba5f4
Paulo Alvarado <commonzenpython@gmail.com>
2013-05-08 03:09:35 -0400

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7"],
-    )
     install_requires=[
         'requests',
     ],
