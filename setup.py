0281699dbd9472c3298c8dd62a347beab279d169
Paulo Alvarado <commonzenpython@gmail.com>
2013-05-08 03:04:23 -0400

    Improved setup.py for PyPi

--- setup.py
+++ setup.py
@@ -4,12 +4,24 @@ from setuptools import setup
 
 setup(
     name="python-amazon-mws",
-    version="0.2",
+    version="0.3",
     description="A python interface for Amazon MWS",
     author="Paulo Alvarado",
     author_email="commonzenpython@gmail.com",
     url="http://github.com/czpython/python-amazon-mws",
     packages=['mws'],
+    download_url = 'https://github.com/czpython/python-amazon-mws/tarball/0.3',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7"],
+    )
     install_requires=[
         'requests',
     ],
