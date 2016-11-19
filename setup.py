5d34edd5b82eff07987529e6b38cf35f9dee39cb
James Hiew <james@hiew.net>
2016-11-19 20:30:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,33 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-
-version = '0.6'
-
-REQUIREMENTS = ['requests']
-
-CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
-    'Environment :: Web Environment',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    'Topic :: Software Development',
-    'Topic :: Software Development :: Libraries :: Application Frameworks',
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.6",
-    "Programming Language :: Python :: 2.7",
-]
+from setuptools import setup
 
 setup(
-    name="python-amazon-mws",
-    version=version,
-    description="A python interface for Amazon MWS",
-    author="Paulo Alvarado",
-    author_email="commonzenpython@gmail.com",
-    url="http://github.com/czpython/python-amazon-mws",
-    packages=find_packages(),
+    name='mws',
+    version='0.6',
+    maintainer="James Hiew",
+    maintainer_email="james@hiew.net",
+    url="http://github.com/jameshiew/mws",
+    description='A python interface for Amazon MWS',
+    packages=['mws'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
     platforms=['OS Independent'],
     license='LICENSE.txt',
-    install_requires=REQUIREMENTS,
-    classifiers=CLASSIFIERS,
     include_package_data=True,
     zip_safe=False
 )
