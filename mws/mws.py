413ad9c3521ee654b102f3ba2c5375333bf13e41
KleinerNull <anstecknadel23@yahoo.de>
2017-06-14 20:22:14 +0200

    Simplified remove_empty
    Instead of mutating a dict building a new one is better. Should be faster than using del and safe. Also casting a set of dict.keys is not necessary, keys in a dict are always unique.

--- mws/mws.py
+++ mws/mws.py
@@ -78,10 +78,7 @@ def remove_empty(d):
     Return:
         dict
     """
-    for key in set(d.keys()):
-        if not d[key]:
-            del d[key]
-    return d
+    return {k: v for k, v in d.items() if not v}
 
 
 def remove_namespace(xml):
