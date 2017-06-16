1e3eafab6687a578da4abfa8acb05b99835e51c7
James Hiew <james@hiew.net>
2017-06-16 19:01:39 +0100

    Add tests for the DataWrapper MD5 checking

---
+++ tests/test_wrappers.py
@@ -0,0 +1,16 @@
+import pytest
+
+from mws.mws import DataWrapper, MWSError
+
+
+def test_content_md5_comparison():
+    data = b'abc\tdef'
+    hash = 'Zj+Bh1BJ8HzBb9ToK28qFQ=='
+    DataWrapper(data, {'content-md5': hash})
+
+
+def test_content_md5_check_raises_exception_if_fails():
+    data = b'abc\tdef'
+    hash = 'notthehash'
+    with pytest.raises(MWSError):
+        DataWrapper(data, {'content-md5': hash})
