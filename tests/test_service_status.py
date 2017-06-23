7ff72ac31dff3ec50731887bf86a3ce9ee98e3ad
ZagrebinB <zagrebin.bogdan@gmail.com>
2017-06-23 16:47:20 +0300

    add requests via proxy and almost of inbound shipment requests

--- tests/test_service_status.py
+++ tests/test_service_status.py
@@ -1,9 +1,16 @@
 import mws
+from mws.mws import InboundShipments
 
 
 def test_get_service_status():
     # we can get the service status without needing API credentials
     # this is a simple smoke test to check that the simplest API request can be successfully made
     orders_api = mws.Orders(access_key='', secret_key='', account_id='')
+    r = orders_api.get_service_status()
+    assert r.response.status_code == 200
+
+
+def test_get_service_status_inbound():
+    orders_api = InboundShipments(access_key='', secret_key='', account_id='')
     r = orders_api.get_service_status()
     assert r.response.status_code == 200
