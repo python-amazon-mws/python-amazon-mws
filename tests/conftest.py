09931cfbba746daf5127b6113187042341e3be3d
James Hiew <james@hiew.net>
2017-08-12 20:49:38 +0100

    Add more pytest fixtures (access_key, secret_key, account_id, timestamp)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,30 @@ import pytest
 
 
 @pytest.fixture
-def credentials():
+def access_key():
+    return "AAAAAAAAAAAAAAAAAAAA"
+
+
+@pytest.fixture
+def secret_key():
+    return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+
+@pytest.fixture
+def account_id():
+    return "AAAAAAAAAAAAAA"
+
+
+@pytest.fixture
+def timestamp():
+    return '2017-08-12T19:40:35Z'
+
+
+@pytest.fixture
+def credentials(access_key, secret_key, account_id):
     """Fake set of MWS credentials"""
     return {
-        "access_key": "AAAAAAAAAAAAAAAAAAAA",
-        "secret_key": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-        "account_id": "AAAAAAAAAAAAAA",
+        "access_key": access_key,
+        "secret_key": secret_key,
+        "account_id": account_id,
     }
