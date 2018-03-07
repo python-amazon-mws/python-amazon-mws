18baea66689111f702c068eb84bc3072c366837a
Alex Hellier <alex@absolutemusic.co.uk>
2018-03-07 10:23:42 +0000

    Update unsafe parameters for new merchant services
    https://docs.python.org/3/tutorial/controlflow.html#default-argument-values

--- mws/mws.py
+++ mws/mws.py
@@ -1362,10 +1362,40 @@ class MerchantFulfillment(MWS):
     VERSION = "2015-06-01"
     NS = '{https://mws.amazonservices.com/MerchantFulfillment/2015-06-01}'
 
-    def get_eligible_shipping_services(self, amazon_order_id=None, seller_orderid=None, item_list=[],
-                                       ship_from_address={}, package_dimensions={}, weight={},
+    def get_eligible_shipping_services(self, amazon_order_id=None, seller_orderid=None, item_list=None,
+                                       ship_from_address=None, package_dimensions=None, weight=None,
                                        must_arrive_by_date=None, ship_date=None,
-                                       shipping_service_options={}, label_customization={}):
+                                       shipping_service_options=None, label_customization=None):
+
+        """
+        http://docs.developer.amazonservices.com/en_UK/merch_fulfill/MerchFulfill_GetEligibleShippingServices.html
+
+        :param amazon_order_id: Required
+        :param seller_orderid:
+        :param item_list: Required
+        :param ship_from_address: Required
+        :param package_dimensions: Required
+        :param weight: Required
+        :param must_arrive_by_date:
+        :param ship_date:
+        :param shipping_service_options: Required
+        :param label_customization:
+        :return:
+        """
+
+        if ship_from_address is None:
+            ship_from_address = {}
+        if package_dimensions is None:
+            package_dimensions = {}
+        if weight is None:
+            weight = {}
+        if item_list is None:
+            item_list = []
+        if shipping_service_options is None:
+            shipping_service_options = {}
+        if label_customization is None:
+            label_customization = {}
+
 
         data = {
             "Action": "GetEligibleShippingServices",
@@ -1382,10 +1412,41 @@ class MerchantFulfillment(MWS):
         data.update(utils.dict_keyed_param("ShipmentRequestDetails.LabelCustomization", label_customization))
         return self.make_request(data)
 
-    def create_shipment(self, amazon_order_id=None, seller_orderid=None, item_list=[], ship_from_address={},
-                        package_dimensions={}, weight={}, must_arrive_by_date=None, ship_date=None,
-                        shipping_service_options={}, label_customization={}, shipping_service_id=None,
+    def create_shipment(self, amazon_order_id=None, seller_orderid=None, item_list=None, ship_from_address=None,
+                        package_dimensions=None, weight=None, must_arrive_by_date=None, ship_date=None,
+                        shipping_service_options=None, label_customization=None, shipping_service_id=None,
                         shipping_service_offer_id=None, hazmat_type=None):
+        """
+        http://docs.developer.amazonservices.com/en_UK/merch_fulfill/MerchFulfill_CreateShipment.html
+
+        :param amazon_order_id: Required
+        :param seller_orderid:
+        :param item_list: Required
+        :param ship_from_address: Required
+        :param package_dimensions: Required
+        :param weight: Required
+        :param must_arrive_by_date:
+        :param ship_date:
+        :param shipping_service_options:
+        :param label_customization:
+        :param shipping_service_id: Required
+        :param shipping_service_offer_id:
+        :param hazmat_type:
+        :return:
+        """
+
+        if item_list is None:
+           item_list = []
+        if ship_from_address is None:
+            ship_from_address = {}
+        if package_dimensions is None:
+            package_dimensions = {}
+        if weight is None:
+            weight = {}
+        if shipping_service_options is None:
+            shipping_service_options = {}
+        if label_customization is None:
+            label_customization = {}
 
         data = {
             "Action": "CreateShipment",
