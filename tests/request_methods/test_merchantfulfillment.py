9ab02d6dd79bc8269a0c454070a74e4608029d1d
Galen Rice <galen.s.rice@gmail.com>
2018-03-13 13:22:47 -0400

    Initial tests for MerchantFulfillment

--- tests/request_methods/test_merchantfulfillment.py
+++ tests/request_methods/test_merchantfulfillment.py
@@ -1,5 +1,5 @@
 """
-Tests for the MWS.MerchantFulfillment API class.
+Tests for the MerchantFulfillment API class.
 """
 import unittest
 import mws
@@ -19,3 +19,43 @@ class MerchantFulfillmentTestCase(unittest.TestCase, CommonRequestTestTools):
             auth_token=self.CREDENTIAL_TOKEN
         )
         self.api._test_request_params = True
+
+    # def test_get_eligible_shipping_services(self):
+    #     """
+    #     XYZ operation.
+    #     """
+    #     params = self.api.get_eligible_shipping_services()
+    #     self.assert_common_params(params)
+    #     assert params['Action'] == 'GetEligibleShippingServices'
+
+    # def test_create_shipment(self):
+    #     """
+    #     XYZ operation.
+    #     """
+    #     params = self.api.create_shipment()
+    #     self.assert_common_params(params)
+    #     assert params['Action'] == 'CreateShipment'
+
+    def test_get_shipment(self):
+        """
+        XYZ operation.
+        """
+        shipment_id = 'UCXN7ZubAj'
+        params = self.api.get_shipment(
+            shipment_id=shipment_id,
+        )
+        self.assert_common_params(params)
+        assert params['Action'] == 'GetShipment'
+        assert params['ShipmentId'] == shipment_id
+
+    def test_cancel_shipment(self):
+        """
+        XYZ operation.
+        """
+        shipment_id = 'C6Pvk0b2yZ'
+        params = self.api.cancel_shipment(
+            shipment_id=shipment_id,
+        )
+        self.assert_common_params(params)
+        assert params['Action'] == 'CancelShipment'
+        assert params['ShipmentId'] == shipment_id
