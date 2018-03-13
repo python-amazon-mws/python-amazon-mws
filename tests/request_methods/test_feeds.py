b388ae344fd12707cec7e4f510c8903699e87b64
Galen Rice <galen.s.rice@gmail.com>
2018-03-13 11:16:18 -0400

    Completing feeds tests

--- tests/request_methods/test_feeds.py
+++ tests/request_methods/test_feeds.py
@@ -39,17 +39,14 @@ class FeedsTestCase(unittest.TestCase, CommonRequestTestTools):
         feed_ids = [
             '1058369303',
             '1228369302',
-            '6987532174',
         ]
         feed_types = [
             '_POST_PRODUCT_DATA_',
             '_POST_FULFILLMENT_ORDER_CANCELLATION_REQUEST_DATA_',
-            '_POST_UIEE_BOOKLOADER_DATA_',
         ]
         processing_statuses = [
-            '_CANCELLED_',
+            '_SUBMITTED_',
             '_AWAITING_ASYNCHRONOUS_REPLY_',
-            '_DONE_',
         ]
         max_count = 698
         params = self.api.get_feed_submission_list(
@@ -67,13 +64,10 @@ class FeedsTestCase(unittest.TestCase, CommonRequestTestTools):
         assert params['MaxCount'] == max_count
         assert params['FeedSubmissionIdList.Id.1'] == feed_ids[0]
         assert params['FeedSubmissionIdList.Id.2'] == feed_ids[1]
-        assert params['FeedSubmissionIdList.Id.3'] == feed_ids[2]
         assert params['FeedTypeList.Type.1'] == feed_types[0]
         assert params['FeedTypeList.Type.2'] == feed_types[1]
-        assert params['FeedTypeList.Type.3'] == feed_types[2]
         assert params['FeedProcessingStatusList.Status.1'] == processing_statuses[0]
         assert params['FeedProcessingStatusList.Status.2'] == processing_statuses[1]
-        assert params['FeedProcessingStatusList.Status.3'] == processing_statuses[2]
 
     def test_get_feed_submission_list_by_next_token(self):
         """
@@ -96,20 +90,74 @@ class FeedsTestCase(unittest.TestCase, CommonRequestTestTools):
         assert params['NextToken'] == next_token
 
     # TODO Complete these later
-    # def test_get_feed_submission_count(self):
-    #     """
-    #     GetFeedSubmissionCount operation
-    #     """
-    #     pass
+    def test_get_feed_submission_count(self):
+        """
+        GetFeedSubmissionCount operation
+        """
+        from_date = datetime.datetime.utcnow()
+        from_date_stamp = from_date.isoformat()
+        to_date = datetime.datetime.utcnow()
+        to_date_stamp = to_date.isoformat()
+        feed_types = [
+            '_POST_PRODUCT_OVERRIDES_DATA_',
+            '_POST_FLAT_FILE_FULFILLMENT_ORDER_CANCELLATION_REQUEST_DATA_',
+        ]
+        processing_statuses = [
+            '_IN_PROGRESS_',
+            '_UNCONFIRMED_',
+        ]
+        params = self.api.get_feed_submission_count(
+            feed_types=feed_types,
+            processing_statuses=processing_statuses,
+            from_date=from_date,
+            to_date=to_date,
+        )
+        self.assert_common_params(params)
+        assert params['Action'] == 'GetFeedSubmissionCount'
+        assert params['SubmittedFromDate'] == from_date_stamp
+        assert params['SubmittedToDate'] == to_date_stamp
+        assert params['FeedTypeList.Type.1'] == feed_types[0]
+        assert params['FeedTypeList.Type.2'] == feed_types[1]
+        assert params['FeedProcessingStatusList.Status.1'] == processing_statuses[0]
+        assert params['FeedProcessingStatusList.Status.2'] == processing_statuses[1]
 
-    # def test_cancel_feed_submissions(self):
-    #     """
-    #     CancelFeedSubmissions operation
-    #     """
-    #     pass
+    def test_cancel_feed_submissions(self):
+        """
+        CancelFeedSubmissions operation
+        """
+        from_date = datetime.datetime.utcnow()
+        from_date_stamp = from_date.isoformat()
+        to_date = datetime.datetime.utcnow()
+        to_date_stamp = to_date.isoformat()
+        feed_ids = [
+            'SUB63kvutS',
+            'l8dM04jxGD',
+        ]
+        feed_types = [
+            '_CANCELLED_',
+            '_DONE_',
+        ]
+        params = self.api.cancel_feed_submissions(
+            feed_ids=feed_ids,
+            feed_types=feed_types,
+            from_date=from_date,
+            to_date=to_date,
+        )
+        self.assert_common_params(params)
+        assert params['Action'] == 'CancelFeedSubmissions'
+        assert params['SubmittedFromDate'] == from_date_stamp
+        assert params['SubmittedToDate'] == to_date_stamp
+        assert params['FeedSubmissionIdList.Id.1'] == feed_ids[0]
+        assert params['FeedSubmissionIdList.Id.2'] == feed_ids[1]
+        assert params['FeedTypeList.Type.1'] == feed_types[0]
+        assert params['FeedTypeList.Type.2'] == feed_types[1]
 
-    # def test_get_feed_submission_result(self):
-    #     """
-    #     GetFeedSubmissionResult operation
-    #     """
-    #     pass
+    def test_get_feed_submission_result(self):
+        """
+        GetFeedSubmissionResult operation
+        """
+        feed_id = 'SJT63jt6M3'
+        params = self.api.get_feed_submission_result(feed_id)
+        self.assert_common_params(params)
+        assert params['Action'] == 'GetFeedSubmissionResult'
+        assert params['FeedSubmissionId'] == feed_id
