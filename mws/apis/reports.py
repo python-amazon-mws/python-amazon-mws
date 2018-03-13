843d8180bac5f5a5ea8a6ed5d873c2a13752cd84
Galen Rice <galen.s.rice@gmail.com>
2018-03-13 15:38:22 -0400

    Argument names updated (PEP8 guidelines)

--- mws/apis/reports.py
+++ mws/apis/reports.py
@@ -25,25 +25,26 @@ class Reports(mws.MWS):
         'GetReportScheduleList',
     ]
 
-    def request_report(self, report_type, start_date=None, end_date=None, marketplaceids=()):
+    def request_report(self, report_type, start_date=None, end_date=None, marketplace_ids=None):
         """
         Creates a report request and submits the request to Amazon MWS.
 
         Docs:
         http://docs.developer.amazonservices.com/en_US/reports/Reports_RequestReport.html
         """
+        marketplace_ids = marketplace_ids or []
         data = {
             'Action': 'RequestReport',
             'ReportType': report_type,
             'StartDate': start_date,
             'EndDate': end_date,
         }
-        data.update(utils.enumerate_param('MarketplaceIdList.Id.', marketplaceids))
+        data.update(utils.enumerate_param('MarketplaceIdList.Id.', marketplace_ids))
         return self.make_request(data)
 
     @next_token_action('GetReportRequestList')
-    def get_report_request_list(self, requestids=(), types=(), processingstatuses=(),
-                                max_count=None, fromdate=None, todate=None, next_token=None):
+    def get_report_request_list(self, request_ids=(), types=(), processing_statuses=(),
+                                max_count=None, from_date=None, to_date=None, next_token=None):
         """
         Returns a list of report requests that you can use to get the ReportRequestId for a report.
 
@@ -55,13 +56,13 @@ class Reports(mws.MWS):
         data = {
             'Action': 'GetReportRequestList',
             'MaxCount': max_count,
-            'RequestedFromDate': fromdate,
-            'RequestedToDate': todate,
+            'RequestedFromDate': from_date,
+            'RequestedToDate': to_date,
         }
         data.update(utils.enumerate_params({
-            'ReportRequestIdList.Id.': requestids,
+            'ReportRequestIdList.Id.': request_ids,
             'ReportTypeList.Type.': types,
-            'ReportProcessingStatusList.Status.': processingstatuses,
+            'ReportProcessingStatusList.Status.': processing_statuses,
         }))
         return self.make_request(data)
 
@@ -74,8 +75,8 @@ class Reports(mws.MWS):
         """
         return self.get_report_request_list(next_token=token)
 
-    def get_report_request_count(self, report_types=(), processingstatuses=(),
-                                 fromdate=None, todate=None):
+    def get_report_request_count(self, report_types=(), processing_statuses=(),
+                                 from_date=None, to_date=None):
         """
         Returns a count of report requests that have been submitted to Amazon MWS for processing.
 
@@ -84,12 +85,12 @@ class Reports(mws.MWS):
         """
         data = {
             'Action': 'GetReportRequestCount',
-            'RequestedFromDate': fromdate,
-            'RequestedToDate': todate,
+            'RequestedFromDate': from_date,
+            'RequestedToDate': to_date,
         }
         data.update(utils.enumerate_params({
             'ReportTypeList.Type.': report_types,
-            'ReportProcessingStatusList.Status.': processingstatuses,
+            'ReportProcessingStatusList.Status.': processing_statuses,
         }))
         return self.make_request(data)
 
@@ -98,8 +99,8 @@ class Reports(mws.MWS):
     #     pass
 
     @next_token_action('GetReportList')
-    def get_report_list(self, requestids=(), max_count=None, types=(), acknowledged=None,
-                        fromdate=None, todate=None, next_token=None):
+    def get_report_list(self, request_ids=(), max_count=None, types=(), acknowledged=None,
+                        from_date=None, to_date=None, next_token=None):
         """
         Returns a list of reports that were created between fromdate and todate
         (defaults to previous 90 days if ommitted).
@@ -112,12 +113,12 @@ class Reports(mws.MWS):
         data = {
             'Action': 'GetReportList',
             'Acknowledged': acknowledged,
-            'AvailableFromDate': fromdate,
-            'AvailableToDate': todate,
+            'AvailableFromDate': from_date,
+            'AvailableToDate': to_date,
             'MaxCount': max_count,
         }
         data.update(utils.enumerate_params({
-            'ReportRequestIdList.Id.': requestids,
+            'ReportRequestIdList.Id.': request_ids,
             'ReportTypeList.Type.': types,
         }))
         return self.make_request(data)
@@ -131,7 +132,7 @@ class Reports(mws.MWS):
         """
         return self.get_report_list(next_token=token)
 
-    def get_report_count(self, report_types=(), acknowledged=None, fromdate=None, todate=None):
+    def get_report_count(self, report_types=(), acknowledged=None, from_date=None, to_date=None):
         """
         Returns a count of the reports, created in the previous 90 days,
         with a status of _DONE_ and that are available for download.
@@ -142,8 +143,8 @@ class Reports(mws.MWS):
         data = {
             'Action': 'GetReportCount',
             'Acknowledged': acknowledged,
-            'AvailableFromDate': fromdate,
-            'AvailableToDate': todate,
+            'AvailableFromDate': from_date,
+            'AvailableToDate': to_date,
         }
         data.update(utils.enumerate_param('ReportTypeList.Type.', report_types))
         return self.make_request(data)
