2851789ae2193f1d9f27cfe92d0be7f6204409e7
Galen Rice <galen.s.rice@gmail.com>
2018-03-14 09:30:02 -0400

    Add tests for `set_ship_from_address` method.

--- tests/request_methods/test_inboundshipments.py
+++ tests/request_methods/test_inboundshipments.py
@@ -8,7 +8,7 @@ from mws.mws import MWSError
 from .utils import CommonRequestTestTools
 
 
-class TestParseItemArgsExceptions(unittest.TestCase):
+class ParseItemArgsTestCase(unittest.TestCase):
     """
     Test cases that ensure `parse_item_args` raises exceptions where appropriate.
     """
@@ -149,7 +149,106 @@ class TestParseItemArgsExceptions(unittest.TestCase):
         self.assertEqual(parsed_items[1], expected[1])
 
 
-class InboundShipmentsTestCase(unittest.TestCase, CommonRequestTestTools):
+class SetShipFromAddressTestCase(unittest.TestCase):
+    """
+    Test case covering msw.InboundShipments.set_ship_from_address
+    """
+    def setUp(self):
+        self.inbound = mws.InboundShipments('', '', '')
+
+    def test_address_empty_raises_exception(self):
+        """
+        Empty address dict should raise MWSError.
+        """
+        address = {}
+        with self.assertRaises(MWSError):
+            self.inbound.set_ship_from_address(address)
+    
+    def test_address_not_dict_raises_exception(self):
+        """
+        Non-dict argument should raise MWSError.
+        """
+        address = 'this is not a dict'
+        with self.assertRaises(MWSError):
+            self.inbound.set_ship_from_address(address)
+    
+    def test_required_keys_missing(self):
+        """
+        Any missing required key should raise MWSError
+        """
+        # Missing name
+        address_1 = {
+            'address_1': '500 Summat Cully Lane',
+            'city': 'Gilead',
+        }
+        # Missing address_1 (address line 1)
+        address_2 = {
+            'name': 'Roland Deschain',
+            'city': 'Gilead',
+        }
+        # Missing city
+        address_3 = {
+            'name': 'Roland Deschain',
+            'address_1': '500 Summat Cully Lane',
+        }
+        with self.assertRaises(MWSError):
+            self.inbound.set_ship_from_address(address_1)
+        with self.assertRaises(MWSError):
+            self.inbound.set_ship_from_address(address_2)
+        with self.assertRaises(MWSError):
+            self.inbound.set_ship_from_address(address_3)
+    
+    def test_full_address_built_properly(self):
+        """
+        An address with all fields covered should be contructed properly.
+        """
+        address = {
+            'name': 'Roland Deschain',
+            'address_1': '500 Summat Cully Lane',
+            'address_2': 'Apartment 19',
+            'city': 'Gilead',
+            'district_or_county': 'West-Town',
+            'state_or_province': 'New Canaan',
+            'postal_code': '13019',
+            'country': 'Mid-World',
+        }
+        self.inbound.set_ship_from_address(address)
+        expected = {
+            'ShipFromAddress.Name': 'Roland Deschain',
+            'ShipFromAddress.AddressLine1': '500 Summat Cully Lane',
+            'ShipFromAddress.AddressLine2': 'Apartment 19',
+            'ShipFromAddress.City': 'Gilead',
+            'ShipFromAddress.DistrictOrCounty': 'West-Town',
+            'ShipFromAddress.StateOrProvinceCode': 'New Canaan',
+            'ShipFromAddress.PostalCode': '13019',
+            'ShipFromAddress.CountryCode': 'Mid-World',
+        }
+        self.assertEqual(self.from_address, expected)
+    
+    def test_partial_address_built_properly(self):
+        """
+        An address with only required fields covered should be contructed properly,
+        with ommitted keys filled in with defaults.
+        """
+        address = {
+            'name': 'Roland Deschain',
+            'address_1': '500 Summat Cully Lane',
+            'city': 'Gilead',
+        }
+        self.inbound.set_ship_from_address(address)
+        expected = {
+            'ShipFromAddress.Name': 'Roland Deschain',
+            'ShipFromAddress.AddressLine1': '500 Summat Cully Lane',
+            'ShipFromAddress.AddressLine2': None,
+            'ShipFromAddress.City': 'Gilead',
+            'ShipFromAddress.DistrictOrCounty': None,
+            'ShipFromAddress.StateOrProvinceCode': None,
+            'ShipFromAddress.PostalCode': None,
+            'ShipFromAddress.CountryCode': 'US',
+        }
+        self.assertEqual(self.from_address, expected)
+
+class InboundShipmentsRequestsTestCase(unittest.TestCase, CommonRequestTestTools):
     """
     Test cases for InboundShipments.
     """
