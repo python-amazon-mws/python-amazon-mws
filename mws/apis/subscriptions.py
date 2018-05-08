c237e8d27f4d9e23ba319d7751f85675c3691458
Alex Hellier <alex@absolutemusic.co.uk>
2018-05-08 21:09:43 +0100

    Subscriptions (#13)
    * Fix delete subscription
    
    * Fix delete subscription
    
    * _type -> notification_type
    
    * small doc update
    
    * change subscriptions attributes_list to be attributes (dictionary) then map the key and value into the correct style for enumeration
    
    * flake8 after merge issues
    
    * missed off subscriptions in updated params

--- mws/apis/subscriptions.py
+++ mws/apis/subscriptions.py
@@ -141,7 +141,7 @@ class Subscriptions(MWS):
                 "Subscription.IsEnabled": str(is_enabled).lower(),
                 "Subscription.NotificationType": notification_type}
 
-        data.update(utils.enumerate_keyed_param("Destination.AttributeList.member", self._parse_attributes(attributes)))
+        data.update(utils.enumerate_keyed_param("Subscription.Destination.AttributeList.member", self._parse_attributes(attributes)))
 
         return self.make_request(data, "POST")
 
@@ -166,7 +166,7 @@ class Subscriptions(MWS):
                 "MarketplaceId": marketplace_id,
                 "Subscription.Destination.DeliveryChannel": delivery_channel,
                 "Subscription.NotificationType": notification_type}
-        data.update(utils.enumerate_keyed_param("Destination.AttributeList.member", self._parse_attributes(attributes)))
+        data.update(utils.enumerate_keyed_param("Subscription.Destination.AttributeList.member", self._parse_attributes(attributes)))
 
         return self.make_request(data, "POST")
 
@@ -233,5 +233,6 @@ class Subscriptions(MWS):
                 "Subscription.Destination.DeliveryChannel": delivery_channel,
                 "Subscription.IsEnabled": str(is_enabled).lower(),
                 "Subscription.NotificationType": notification_type}
-        data.update(utils.enumerate_keyed_param("Destination.AttributeList.member", self._parse_attributes(attributes)))
+
+        data.update(utils.enumerate_keyed_param("Subscription.Destination.AttributeList.member", self._parse_attributes(attributes)))
         return self.make_request(data, "POST")
