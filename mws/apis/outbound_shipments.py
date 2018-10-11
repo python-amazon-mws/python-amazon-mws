ac5df7bf4310fac15c907dea9b64f8e8b012aaca
tgehrs <tyler@abreezetech.com>
2018-10-11 10:42:25 -0400

    Add OutboundShipments.create_fulfillment_order
    This is continuation of https://github.com/python-amazon-mws/python-amazon-mws/pull/95

--- mws/apis/outbound_shipments.py
+++ mws/apis/outbound_shipments.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 # import warnings
 
 from ..mws import MWS
-# from .. import utils
+from .. import utils
 from ..decorators import next_token_action
 
 
@@ -32,15 +32,52 @@ class OutboundShipments(MWS):
         """
         raise NotImplementedError
 
-    def create_fulfillment_order(self):
+    def create_fulfillment_order(self, seller_fulfillment_order_id, displayable_order_id,
+                                displayable_order_datetime, displayable_order_comment, shipping_speed_category,
+                                destination_address, items, marketplace_id=None, fulfillment_action=None,
+                                fulfillment_policy=None, notification_email_list=None, cod_settings=None,
+                                delivery_window=None):
+
         """
         Requests that Amazon ship items from the seller's inventory in Amazon's fulfillment network
         to a destination address.
 
+        :param marketplace_id:
+        :param seller_fulfillment_order_id: Required
+        :param fulfillment_action:
+        :param displayable_order_id: Required
+        :param displayable_order_datetime: Required
+        :param displayable_order_comment: Required
+        :param shipping_speed_category: Required
+        :param destination_address: Required
+        :param fulfillment_policy:
+        :param notification_email_list:
+        :param cod_settings:
+        :param items: Required
+        :param delivery_window:
+
         Docs:
         http://docs.developer.amazonservices.com/en_US/fba_outbound/FBAOutbound_CreateFulfillmentOrder.html
         """
-        raise NotImplementedError
+        data = {
+            "Action": "CreateFulfillmentOrder",
+            "MarketplaceId": marketplace_id,
+            "SellerFulfillmentOrderId": seller_fulfillment_order_id,
+            "FulfillmentAction": fulfillment_action,
+            "DisplayableOrderId": displayable_order_id,
+            "DisplayableOrderDateTime": displayable_order_datetime,
+            "DisplayableOrderComment": displayable_order_comment,
+            "ShippingSpeedCategory": shipping_speed_category,
+            "FulfillmentPolicy": fulfillment_policy,
+        }
+
+        data.update(utils.enumerate_keyed_param("Items.member", items or []))
+        data.update(utils.dict_keyed_param("DestinationAddress", destination_address or {}))
+        data.update(utils.dict_keyed_param("CODSettings", cod_settings or {}))
+        data.update(utils.dict_keyed_param("DeliveryWindow", delivery_window or {}))
+        data.update(utils.enumerate_param("NotificationEmailList.member", notification_email_list or []))
+
+        return self.make_request(data)
 
     def update_fulfillment_order(self):
         """
