19275bf632d3f235ab85046e04eae7a38ffa59dd
Tyler Matteson <tyler@agritheory.com>
2020-02-27 09:08:06 -0500

    add all current marketplaces and alphabetize by country code

--- mws/mws.py
+++ mws/mws.py
@@ -37,17 +37,20 @@ class Marketplaces(Enum):
     AU = ('https://mws.amazonservices.com.au', 'A39IBJ37TRP1C6')
     BR = ('https://mws.amazonservices.com', 'A2Q3Y263D00KWC')
     CA = ('https://mws.amazonservices.ca', 'A2EUQ1WTGCTBG2')
-    CN = ('https://mws.amazonservices.com.cn', 'AAHKV2X7AFYLW')
     DE = ('https://mws-eu.amazonservices.com', 'A1PA6795UKMFR9')
+    EG = ('https://mws-eu.amazonservices.com', 'ARBP9OOSHTCHU')
     ES = ('https://mws-eu.amazonservices.com', 'A1RKKUPIHCS9HS')
     FR = ('https://mws-eu.amazonservices.com', 'A13V1IB3VIYZZH')
+    GB = ('https://mws-eu.amazonservices.com', 'A1F83G8C2ARO7P')
     IN = ('https://mws.amazonservices.in', 'A21TJRUUN4KGV')
     IT = ('https://mws-eu.amazonservices.com', 'APJ6JRA9NG5V4')
     JP = ('https://mws.amazonservices.jp', 'A1VC38T7YXB528')
     MX = ('https://mws.amazonservices.com.mx', 'A1AM78C64UM0Y8')
-    UK = ('https://mws-eu.amazonservices.com', 'A1F83G8C2ARO7P')
-    US = ('https://mws.amazonservices.com', 'ATVPDKIKX0DER')
     NL = ('https://mws-eu.amazonservices.com', 'A1805IZSGTT6HS')
+    SA = ('https://mws-eu.amazonservices.com', 'A17E79C6D8DWNP')
+    SG = ('https://mws-fe.amazonservices.com', 'A19VAU5U5O7RUS')
+    TR = ('https://mws-eu.amazonservices.com', 'A33AVAJ2PDY3EV')
+    US = ('https://mws.amazonservices.com', 'ATVPDKIKX0DER')
 
     def __init__(self, endpoint, marketplace_id):
         """Easy dot access like: Marketplaces.endpoint ."""
