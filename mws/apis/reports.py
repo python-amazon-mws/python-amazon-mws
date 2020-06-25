99ff5e913a3956e9e9d49d5efff599bd7280b392
Galen Rice <galen.s.rice@gmail.com>
2020-06-25 11:37:35 -0400

    Completed `update_report_acknowledgements`
    Added `acknowledged` boolean parameter; cleaned up dict structure

--- mws/apis/reports.py
+++ mws/apis/reports.py
@@ -215,15 +215,21 @@ class Reports(MWS):
         data.update(utils.enumerate_param('ReportTypeList.Type.', report_types))
         return self.make_request(data)
 
-    def update_report_acknowledgements(self, report_ids=()):
+    def update_report_acknowledgements(self, report_ids=None, acknowledged=None):
         """
         Updates the acknowledged status of one or more reports
 
         Docs:
         http://docs.developer.amazonservices.com/en_US/reports/Reports_UpdateReportAcknowledgements.html
         """
-        data = dict(Action='UpdateReportAcknowledgements') 
-        data.update(utils.enumerate_param('ReportIdList.Id.', report_ids)) 
+        report_ids = report_ids or []
+        if acknowledged is not None:
+            acknowledged = str(bool(acknowledged)).lower()
+        data = {
+            "Action": "UpdateReportAcknowledgements",
+            "Acknowledged": acknowledged,
+        }
+        data.update(utils.enumerate_param("ReportIdList.Id.", report_ids)) 
         return self.make_request(data)
 
 
