dea20c01ab8901b7b32a0de76d0d493c77d37a4c
Galen Rice <galen.s.rice@gmail.com>
2020-07-16 09:49:03 -0400

    Remove support for <3.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 import sys
 
-version = "1.0.0dev11"
+version = "1.0.0dev12"
 homepage = "https://github.com/python-amazon-mws/python-amazon-mws"
 short_description = "Python library for interacting with the Amazon MWS API"
 with open("README.md") as readme:
@@ -45,11 +45,7 @@ setuptools.setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
