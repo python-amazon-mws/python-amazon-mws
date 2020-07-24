48e099a22a6075543d88feb0e98e11465b43ed9c
Tomas Neme <lacrymology@gmail.com>
2020-07-24 19:21:06 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 import sys
 
-version = "1.0.0dev12"
+version = "1.0.0dev13"
 homepage = "https://github.com/python-amazon-mws/python-amazon-mws"
 short_description = "Python library for interacting with the Amazon MWS API"
 with open("README.md") as readme:
