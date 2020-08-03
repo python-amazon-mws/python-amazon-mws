d2eebf19342e70d883cb39ab4234f2e010c05cc1
Galen Rice <galen.s.rice@gmail.com>
2020-08-03 14:34:11 -0400

    generic request method and `RequestParameter` The new `RequestParameter` class will be leveraged in a refactor once current PRs have calmed down.

--- mws/mws.py
+++ mws/mws.py
@@ -13,6 +13,7 @@ import re
 import warnings
 from zipfile import ZipFile
 from io import BytesIO
+from collections.abc import Iterable
 
 from requests import request
 from requests.exceptions import HTTPError
@@ -400,3 +401,205 @@ class MWS(object):
             "`utils.enumerate_params` for multiple params."
         ), DeprecationWarning)
         return utils.enumerate_param(param, values)
+
+    def generic_request(self, action, parameters=None, method="GET", **kwargs):
+        """Builds a generic request with arbitrary parameter arguments.
+
+        `action` is a string matching the name of the request action
+        (i.e. "ListOrders").
+
+        `parameters` must be a dict, and is passed to `RequestParameter` as `value`
+        (no `key` arg is used). See docs for `RequestParameter` for details.
+
+        Set `method` to `"POST"` to send this request via POST. Defaults to `"GET"`.
+
+        `kwargs` are passed unchanged to `make_request`.
+        """
+        if not self.uri or self.uri == "/":
+            raise ValueError(
+                (
+                    "Cannot send generic request to URI '%s'. "
+                    "Please use one of the API classes "
+                    "(`mws.apis.Reports`, `mws.apis.Feeds`, etc.) "
+                    "to initiate this request."
+                )
+                % self.uri
+            )
+        if not isinstance(parameters, dict):
+            raise ValueError("`parameters` must be a dict.")
+        data = {"Action": action}
+        data.update(RequestParameter(value=parameters))
+        self.make_request(data, method=method, **kwargs)
+
+
+# TODO Move this to its own module when things are being reworked!
+class RequestParameter:
+    """An MWS request parameter, defined by a `key` string and a `value`.
+
+    Using this object with its `to_dict` method, any arbitrarily-nested list or dict
+    in `value` will be expanded and flattened, such that each key in a sub-dict is
+    concatenated to `key` as a dotted string; and each element in a list is
+    enumerated (starting from 1) and concatenated to `key`, also as a dotted string.
+
+    Example:
+        value = {
+            "a": 1,
+            "b": "hello",
+            "c": [
+                "foo",
+                "bar",
+                {
+                    "what": "have",
+                    "you": [
+                        5,
+                        6,
+                        7,
+                    ],
+                },
+            ],
+        }
+        print(RequestParameter(key="example", value=value).to_dict())
+        # Formatted for readability:
+        >>> {
+            "example.a": 1,
+            "example.b": "hello",
+            "example.c.1": "foo",
+            "example.c.2": "bar",
+            "example.c.3.what": "have",
+            "example.c.3.you.1": 5,
+            "example.c.3.you.2": 6,
+            "example.c.3.you.3": 7,
+        }
+
+    - The parameter key "example" is placed in front of each new key.
+      - An empty `key` can also be used when `value` is a nested object:
+        `RequestParameter(value=value)`.
+        This will output the same as above, without `example.` in front of each key.
+      - When using an empty `key`, having a `value` that is not a dict and
+        not a non-string iterable raises ValueError
+    - "a" and "b" are simple values, and are returned.
+    - "c" contains an iterable (list), which is enumerated with a 1-based index.
+      These are joined to "c" with ".", creating keys "c.1" and "c.2".
+    - At "c.3", another nested object is located. This is passed recursively to a new
+      `RequestParameter`, and the same process repeats (dicts are keyed, iterables
+      are enumerated with 1-based index, and simple values are returned).
+    - The same occurs for "c.3.you", where an iterable is found and is enumerated.
+    - The final output should always be a flat dictionary with key-value pairs.
+
+    The output of `to_dict` is used within the `to_str` method to create a single
+    string value from all key-value pairs (keys and values joined by "=",
+    and pairs joined by "&").
+    """
+
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.validate()
+
+    def validate(self):
+        if not self.key and not self._val_is_dict() and not self._val_is_iterable():
+            raise ValueError(
+                "Parameter with empty `key` must have a dict or iterable `value`."
+            )
+
+    def _val_is_str(self):
+        """Return bool, whether the value is a string.
+
+        Used solely in the `_val_is_iterable` test.
+        """
+        return isinstance(self.value, str)
+
+    def _val_is_dict(self):
+        """Return bool, whether the value is a dict."""
+        return isinstance(self.value, dict)
+
+    def _val_is_iterable(self):
+        """Return bool, whether the value is a "psuedo-iterable".
+
+        As a special case, returns False if the value is a dict or str,
+        because we want to treat those objects differently.
+        """
+        if self._val_is_dict() or self._val_is_str():
+            return False
+        return isinstance(self.value, Iterable)
+
+    def to_dict(self):
+        """Return a flat dict, 1 level deep, by enumerating or keying nested
+        lists and dicts in `self.value`.
+        """
+        if self.value is None:
+            # Returns nothing for a `None` value
+            return {}
+        if self._val_is_dict():
+            return self.keyed_value()
+        if self._val_is_iterable():
+            return self.enumerated_value()
+        return {self.key: self.value}
+
+    def to_str(self):
+        """Converts the output of `to_dict` to a string.
+
+        Each key-value pair in the flattened dict is output as "key=val",
+        and all pairs are joined by "&".
+        """
+        content = self.to_dict()
+        output = []
+        for key, val in content.items():
+            output.append("{}={}".format(key, val))
+        return "&".join(output)
+
+    @property
+    def param_key(self):
+        """Outputs the key to use when outputting nested parameters.
+
+        If the key is not set, returns an empty string.
+        Otherwise returns the key string, ensuring there is a "." appended to it.
+        """
+        param = ""
+        if self.key:
+            param = self.key
+            if not param.endswith("."):
+                param += "."
+        return param
+
+    def keyed_value(self):
+        """Returns a flat dict for a nested dict `value`.
+
+        For each `sub_key`/`sub_val` pair of the `value` dict,
+        `sub_key` is combined with this parameter's `key` and "." to create a new key.
+
+        This new key and `sub_val` are then passed to a new `RequestParameter` object,
+        where the output of that sub-parameter's `to_dict()` method is recursively
+        added back to the return value dictionary here.
+        """
+        if not self._val_is_dict:
+            raise ValueError("Cannot generate keyed value for non-dict `value`.")
+        output = {}
+        for sub_key, val in self.value.items():
+            new_key = "{}{}".format(self.param_key, sub_key)
+            # Update our output with the dict from a nested parameter
+            # using this new key and value.
+            output.update(RequestParameter(key=new_key, value=val).to_dict())
+        return output
+
+    def enumerated_value(self):
+        """Returns a flat dict for a nested iterable `value` (similar to `keyed_value`).
+
+        Each element of the `value` iterable is enumerated with a 1-based index `idx`.
+        `idx` is then joined to `key` with "." to obtain a new key.
+
+        From there, the same recursive methodology as `keyed_value` is used,
+        generating a sub-parameter and dict output that is added back
+        to the return value dictionary.
+        """
+        if not self._val_is_iterable:
+            raise ValueError(
+                "Cannot generate enumerated value for non-iterable `value`."
+            )
+        output = {}
+        for idx, val in enumerate(self.value):
+            new_key = "{}{}".format(self.param_key, idx + 1)
+            # Update our output with the dict from a nested parameter
+            # using this new key and value.
+            output.update(RequestParameter(key=new_key, value=val).to_dict())
+        return output
