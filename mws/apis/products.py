1c03c6b5b671bd16d63f60aa4cef276a0097ecad
Ryan Daly <49821564+Ryan-Daly@users.noreply.github.com>
2020-09-25 21:25:38 +0100

    Added get_my_fees_estimate() to products.py
    Saw there wasn't a get my fees estimate. Function arguments must be lists but, leaves it open to make 20 requests all different to one another or, for example the same marketplace and currency but everything else different.

--- mws/apis/products.py
+++ mws/apis/products.py
@@ -163,9 +163,89 @@ class Products(MWS):
         }
         return self.make_request(data)
 
-    # # # TODO add this
-    # def get_my_fees_estimate(self):
-    #     pass
+    def get_my_fees_estimate(
+        self, marketplace_id, id_type, id_value, is_amazon_fulfilled, identifier, listing_price, listing_price_currency_code
+    ):
+
+        """
+        Each argument must be a list, up to a length of 20, with id_value (number of ASINs or SellerSKUs) determining how many requests are made.
+        Options: 
+            A single element list which repeats, or
+            A list of equal length as id_value for a one to one relationship
+
+        Example:
+            marketplace_id = ['A1F83G8C2ARO7P']
+            id_value = ['B082YTKC47', 'B07WGJ9JGP', 'B01LXP5TXI']
+            id_type = ['ASIN']
+            is_amazon_fulfilled = [True, False, True]
+            identifier = ['request1', 'request2', 'request3']
+            listing_price = [9, 16.42, 2.99]
+            listing_price_currency_code = ['GBP']
+
+        Example 2:
+            marketplace_id = ['A1F83G8C2ARO7P', 'ATVPDKIKX0DER', 'A13V1IB3VIYZZH']
+            id_value = ['B082YTKC47', 'B07WGJ9JGP', 'B01-LXP-TXI6']
+            id_type = ['ASIN', 'ASIN', 'SellerSKU']
+            is_amazon_fulfilled = [True]
+            identifier = ['Fee requests']
+            listing_price = [19]
+            listing_price_currency_code = ['GBP', 'USD', 'EUR']
+            
+            
+        Returns Amazon Fees for ASINs and SellerSKUs specified.
+
+        Docs:
+        https://docs.developer.amazonservices.com/en_UK/products/Products_GetMyFeesEstimate.html
+
+        """
+        
+        data = {
+            "Action": "GetMyFeesEstimate",
+        }
+
+        values = []
+        for i in range(len(id_value)):
+            values_dict = dict()
+
+            try:
+                values_dict['MarketplaceId'] = marketplace_id[i]
+            except IndexError:
+                values_dict['MarketplaceId'] = marketplace_id[0]
+                
+            try:
+                values_dict['IdType'] = id_type[i]
+            except IndexError:
+                values_dict['IdType'] = id_type[0]
+            
+            try:
+                values_dict['IdValue'] = id_value[i]
+            except IndexError:
+                values_dict['IdValue'] = id_value[0]
+
+            try:
+                values_dict['IsAmazonFulfilled'] = is_amazon_fulfilled[i]
+            except IndexError:
+                values_dict['IsAmazonFulfilled'] = is_amazon_fulfilled[0]
+            
+            try:
+                values_dict['Identifier'] = identifier[i]
+            except IndexError:
+                values_dict['Identifier'] = identifier[0]
+            
+            try:
+                values_dict['PriceToEstimateFees.ListingPrice.Amount'] = listing_price[i]
+            except IndexError:
+                values_dict['PriceToEstimateFees.ListingPrice.Amount'] = listing_price[0]
+
+            try:
+                values_dict['PriceToEstimateFees.ListingPrice.CurrencyCode'] = listing_price_currency_code[i]
+            except IndexError:
+                values_dict['PriceToEstimateFees.ListingPrice.CurrencyCode'] = listing_price_currency_code[0]
+            
+            values.append(values_dict)
+
+        data.update(utils.enumerate_keyed_param("FeesEstimateRequestList.FeesEstimateRequest.", values))
+        return self.make_request(data)
 
     def get_my_price_for_sku(self, marketplace_id, skus, condition=None):
         """
