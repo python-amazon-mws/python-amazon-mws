5b2519d74c3e97004a151c0d17fe03960ebf6104
Alex Hellier <alex@absolutemusic.co.uk>
2020-12-18 12:00:24 +0000

    Working example of the get_my_fees_estimate
    along with the resulting XML

--- examples/get_fees.py
+++ examples/get_fees.py
@@ -10,6 +10,50 @@ SECRET_KEY = None
 ACCOUNT_ID = None
 
 
+def find_asin(amazon_type="ASIN", product="B07QR73T66"):
+    """
+    Find a specific product by ASIN - note the use of a list here.
+
+    We are setting some defaults for the amazon_type and product here, they are ASIN and the
+    relevant ASIN for a focusrite saffire audio interface
+
+    """
+
+    # Make sure we send a LIST of ids in.
+    if not isinstance(product, list):
+        product = [product]
+
+    # Call the products api and get find products in amazons catalogue based on our query parameters.
+    # Notice how we are using our Marketplaces enum to send in the correct marketplace ID
+    results = products_api.get_matching_product_for_id(
+        marketplace_id=Marketplaces.UK.marketplace_id, type_=amazon_type, ids=product
+    )
+
+    print(
+        "ORIGINAL XML RESPONSE - \
+        use this for debugging or raising issues with - check for confidential information first."
+    )
+    print(results.original)
+
+    print("The dict like (ObjectDict) response")
+    print(results.parsed)
+
+    # the .parsed node is not guaranteed to be a list so convert it if not.
+    if not isinstance(results.parsed, list):
+        found_products = [results.parsed]
+    else:
+        found_products = results.parsed
+
+    # Output some useful data showing dot notation of the ObjectDict.
+    for example in found_products:
+        print("The product title")
+        print(example.Products.Product.AttributeSets.ItemAttributes.Title)
+        print("The category")
+        print(example.Products.Product.SalesRankings.SalesRank[0].ProductCategoryId)
+        print("The current sales rank")
+        print(example.Products.Product.SalesRankings.SalesRank[0].Rank)
+
+
 def get_fees():
     """
     Get amazon fees estimate for a single product
