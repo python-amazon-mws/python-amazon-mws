eaeb744255a2e4490110eb72b48243df8b326be8
Galen Rice <galen.s.rice@gmail.com>
2021-01-27 04:29:01 +0000

    Type annotations added to Reports API.
    Further, docstrings for methods updated to follow style
    in InboundShipments and elsewhere.

--- mws/apis/reports.py
+++ mws/apis/reports.py
@@ -1,7 +1,10 @@
 """Amazon MWS Reports API."""
 from enum import Enum
+import datetime
+import typing
+from typing import List, Union
 
-from mws import MWS
+from mws import MWS, Marketplaces
 from mws.utils.params import enumerate_param
 from mws.utils.params import enumerate_params
 from mws.utils.params import coerce_to_bool
@@ -10,19 +13,20 @@ from mws.decorators import next_token_action
 # DEPRECATIONS
 from mws.utils.deprecation import kwargs_renamed_for_v11
 
-# TODO Add Schedule enumerations as constants
+
+DateType = Union["datetime.datetime", "datetime.date"]
 
 
-def report_options_str(report_options):
+def report_options_str(report_options: dict) -> str:
     """Given a set of `report_options` as a dict,
     converts those options to a URL-encoded string.
 
     Each key-value pair in the dict is presented as "key=value", which is then URL-encoded
-    to, for instance, "key%3Dvalue".
-    Key-value pairs are then joined with ";".
+    to, for instance, "key%3Dvalue". Key-value pairs are then joined with ";".
 
-    See examples in ReportType_Enumeration docs for details:
-    https://docs.developer.amazonservices.com/en_US/reports/Reports_ReportType.html
+    See examples in the
+    `ReportType_Enumeration docs <https://docs.developer.amazonservices.com/en_US/reports/Reports_ReportType.html>`_
+    for details.
     """
     if not report_options:
         return None
@@ -45,8 +49,8 @@ def report_options_str(report_options):
 class Reports(MWS):
     """Amazon MWS Reports API.
 
-    Docs:
-    https://docs.developer.amazonservices.com/en_US/reports/Reports_Overview.html
+    `MWS Docs: Reports API Overview
+    <https://docs.developer.amazonservices.com/en_US/reports/Reports_Overview.html>`_
     """
 
     URI = "/Reports/2009-01-01"
@@ -60,16 +64,16 @@ class Reports(MWS):
     @kwargs_renamed_for_v11([("marketplaceids", "marketplace_ids")])
     def request_report(
         self,
-        report_type,
-        start_date=None,
-        end_date=None,
-        marketplace_ids=None,
-        report_options=None,
+        report_type: Union["ReportType", str],
+        start_date: DateType = None,
+        end_date: DateType = None,
+        marketplace_ids: List[Union[Marketplaces, str]] = None,
+        report_options: dict = None,
     ):
         """Creates a report request and submits the request to Amazon MWS.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_RequestReport.html
+        `MWS Docs: RequestReport
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_RequestReport.html>`_
         """
         marketplace_ids = marketplace_ids or []
         report_options = report_options or {}
@@ -94,21 +98,22 @@ class Reports(MWS):
     @next_token_action("GetReportRequestList")
     def get_report_request_list(
         self,
-        request_ids=None,
-        report_types=None,
-        processing_statuses=None,
-        max_count=None,
-        from_date=None,
-        to_date=None,
-        next_token=None,
+        request_ids: List[str] = None,
+        report_types: List[Union["ReportType", str]] = None,
+        processing_statuses: List[str] = None,
+        max_count: int = None,
+        from_date: DateType = None,
+        to_date: DateType = None,
+        next_token: str = None,
     ):
         """Returns a list of report requests that you can use
         to get the ReportRequestId for a report.
 
-        Pass `next_token` to call "GetReportRequestListByNextToken" instead.
+        Pass ``next_token`` with no other arguments to call the
+        **GetReportRequestListByNextToken** operation, requesting the next page of results.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportRequestList.html
+        `MWS Docs: GetReportRequestList
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportRequestList.html>`_
         """
         request_ids = request_ids or []
         report_types = report_types or []
@@ -129,11 +134,11 @@ class Reports(MWS):
         )
         return self.make_request("GetReportRequestList", data)
 
-    def get_report_request_list_by_next_token(self, token):
-        """Alias for `get_report_request_list(next_token=token)`.
+    def get_report_request_list_by_next_token(self, token: str):
+        """Alias for ``get_report_request_list(next_token=token)``.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportRequestListByNextToken.html
+        `MWS Docs: GetReportRequestListByNextToken
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportRequestListByNextToken.html>`_
         """
         return self.get_report_request_list(next_token=token)
 
@@ -145,13 +150,17 @@ class Reports(MWS):
         ]
     )
     def get_report_request_count(
-        self, report_types=None, processing_statuses=None, from_date=None, to_date=None
+        self,
+        report_types: List[Union["ReportType", str]] = None,
+        processing_statuses: List[str] = None,
+        from_date: DateType = None,
+        to_date: DateType = None,
     ):
         """Returns a count of report requests that have been submitted
         to Amazon MWS for processing.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportRequestCount.html
+        `MWS Docs: GetReportRequestCount
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportRequestCount.html>`_
         """
         report_types = report_types or []
         processing_statuses = processing_statuses or []
@@ -184,21 +193,22 @@ class Reports(MWS):
     @next_token_action("GetReportList")
     def get_report_list(
         self,
-        request_ids=None,
-        max_count=None,
-        report_types=None,
-        acknowledged=None,
-        from_date=None,
-        to_date=None,
-        next_token=None,
+        request_ids: List[str] = None,
+        max_count: int = None,
+        report_types: List[Union["ReportType", str]] = None,
+        acknowledged: bool = None,
+        from_date: DateType = None,
+        to_date: DateType = None,
+        next_token: str = None,
     ):
         """Returns a list of reports that were created between fromdate and todate
         (defaults to previous 90 days if ommitted).
 
-        Pass `next_token` to call "GetReportListByNextToken" instead.
+        Pass ``next_token`` with no other arguments to call the
+        **GetReportListByNextToken** operation, requesting the next page of results.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportList.html
+        `MWS Docs: GetReportList
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportList.html>`_
         """
         request_ids = request_ids or []
         report_types = report_types or []
@@ -220,23 +230,27 @@ class Reports(MWS):
         )
         return self.make_request("GetReportList", data)
 
-    def get_report_list_by_next_token(self, token):
-        """Alias for `get_report_list(next_token=token)`.
+    def get_report_list_by_next_token(self, token: str):
+        """Alias for ``get_report_list(next_token=token)``.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportListByNextToken.html
+        `MWS Docs: GetReportListByNextToken
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportListByNextToken.html>`_
         """
         return self.get_report_list(next_token=token)
 
     @kwargs_renamed_for_v11([("fromdate", "from_date"), ("todate", "to_date")])
     def get_report_count(
-        self, report_types=None, acknowledged=None, from_date=None, to_date=None
+        self,
+        report_types: List[Union["ReportType", str]] = None,
+        acknowledged: bool = None,
+        from_date: DateType = None,
+        to_date: DateType = None,
     ):
         """Returns a count of the reports, created in the previous 90 days,
         with a status of _DONE_ and that are available for download.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportCount.html
+        `MWS Docs: GetReportCount
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportCount.html>`_
         """
         report_types = report_types or []
         if acknowledged is not None:
@@ -249,11 +263,11 @@ class Reports(MWS):
         data.update(enumerate_param("ReportTypeList.Type.", report_types))
         return self.make_request("GetReportCount", data)
 
-    def get_report(self, report_id):
+    def get_report(self, report_id: str):
         """Returns the contents of a report and the Content-MD5 header for the returned report body.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReport.html
+        `MWS Docs: GetReport
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReport.html>`_
         """
         return self.make_request("GetReport", {"ReportId": report_id})
 
@@ -263,43 +277,55 @@ class Reports(MWS):
 
     @kwargs_renamed_for_v11([("types", "report_types")])
     @next_token_action("GetReportScheduleList")
-    def get_report_schedule_list(self, report_types=None, next_token=None):
+    def get_report_schedule_list(
+        self,
+        report_types: List[Union["ReportType", str]] = None,
+        next_token: str = None,
+    ):
         """Returns a list of order report requests that are scheduled
         to be submitted to Amazon MWS for processing.
 
-        Pass `next_token` to call "GetReportScheduleListByNextToken" instead.
+        Pass ``next_token`` with no other arguments to call the
+        **GetReportScheduleListByNextToken** operation, requesting the next page of results.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportScheduleList.html
+        `MWS Docs: GetReportScheduleList
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportScheduleList.html>`_
         """
         report_types = report_types or []
         data = enumerate_param("ReportTypeList.Type.", report_types)
         return self.make_request("GetReportScheduleList", data)
 
-    def get_report_schedule_list_by_next_token(self, token):
-        """Alias for `get_report_schedule_list(next_token=token)`.
+    def get_report_schedule_list_by_next_token(self, token: str):
+        """Alias for ``get_report_schedule_list(next_token=token)``.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportScheduleListByNextToken.html
+        `MWS Docs: GetReportScheduleListByNextToken
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportScheduleListByNextToken.html>`_
         """
         return self.get_report_schedule_list(next_token=token)
 
     @kwargs_renamed_for_v11([("types", "report_types")])
-    def get_report_schedule_count(self, report_types=None):
+    def get_report_schedule_count(
+        self,
+        report_types: List[Union["ReportType", str]] = None,
+    ):
         """Returns a count of order report requests that are scheduled to be submitted to Amazon MWS.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportScheduleCount.html
+        `MWS Docs: GetReportScheduleCount
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_GetReportScheduleCount.html>`_
         """
         report_types = report_types or []
         data = enumerate_param("ReportTypeList.Type.", report_types)
         return self.make_request("GetReportScheduleCount", data)
 
-    def update_report_acknowledgements(self, report_ids=None, acknowledged=None):
+    def update_report_acknowledgements(
+        self,
+        report_ids: List[str] = None,
+        acknowledged: bool = None,
+    ):
         """Updates the acknowledged status of one or more reports.
 
-        Docs:
-        https://docs.developer.amazonservices.com/en_US/reports/Reports_UpdateReportAcknowledgements.html
+        `MWS Docs: UpdateReportAcknowledgements
+        <https://docs.developer.amazonservices.com/en_US/reports/Reports_UpdateReportAcknowledgements.html>`_
         """
         report_ids = report_ids or []
         if acknowledged is not None:
@@ -309,12 +335,17 @@ class Reports(MWS):
         return self.make_request("UpdateReportAcknowledgements", data)
 
 
-class ReportType(Enum):
-    """Better names for reports."""
+class ReportType(str, Enum):
+    """An enumeration of the types of reports that can be requested from Amazon MWS.
+
+    `MWS Docs: ReportType enumeration
+    <https://docs.developer.amazonservices.com/en_US/reports/Reports_ReportType.html>`_
+    """
 
     # Listing Reports
     INVENTORY = "_GET_FLAT_FILE_OPEN_LISTINGS_DATA_"
     ACTIVE_LISTINGS = "_GET_MERCHANT_LISTINGS_DATA_"
+    ALL_LISTINGS = "_GET_MERCHANT_LISTINGS_ALL_DATA_"
     OPEN_LISTINGS = "_GET_MERCHANT_LISTINGS_DATA_BACK_COMPAT_"
     OPEN_LISTINGS_LITE = "_GET_MERCHANT_LISTINGS_DATA_LITE_"
     OPEN_LISTINGS_LITER = "_GET_MERCHANT_LISTINGS_DATA_LITER_"
@@ -392,3 +423,91 @@ class ReportType(Enum):
     FBA_REMOVAL_RECOMMENDED = "_GET_FBA_RECOMMENDED_REMOVAL_DATA_"
     FBA_REMOVAL_ORDER_DETAIL = "_GET_FBA_FULFILLMENT_REMOVAL_ORDER_DETAIL_DATA_"
     FBA_REMOVAL_SHIPMENT_DETAIL = "_GET_FBA_FULFILLMENT_REMOVAL_SHIPMENT_DETAIL_DATA_"
+
+
+class Schedule(str, Enum):
+    """An enumeration of the units of time that reports can be requested.
+
+    `MWS Docs: Schedule enumeration
+    <https://docs.developer.amazonservices.com/en_US/reports/Reports_Schedule.html>`_
+    """
+
+    # I've introduced what I think are useful names for these schedules,
+    # but Amazon didn't seem to have a clear set of consistent naming rules.
+    # So, I opted for a lot of aliases, providing several consistent series of options.
+    # There are singular and plurals, minutes spelled out vs min, "weekly" as well as
+    # "7 days" and "every 1 week", etc.
+    # Weird bit at the 3-day mark where MWS chose to say "72 hours", but whatever.
+    # Aliases are cheap, so here we go.
+
+    # 15 minutes
+    EVERY_15_MIN = "_15_MINUTES_"
+    EVERY_15_MINS = "_15_MINUTES_"
+    EVERY_15_MINUTE = "_15_MINUTES_"
+    EVERY_15_MINUTES = "_15_MINUTES_"
+    # 30 minutes
+    EVERY_30_MIN = "_30_MINUTES_"
+    EVERY_30_MINS = "_30_MINUTES_"
+    EVERY_30_MINUTE = "_30_MINUTES_"
+    EVERY_30_MINUTES = "_30_MINUTES_"
+    # Hourly
+    EVERY_HOUR = "_1_HOUR_"
+    EVERY_1_HOUR = "_1_HOUR_"
+    EVERY_1_HOURS = "_1_HOUR_"
+    # 2 hours
+    EVERY_2_HOUR = "_2_HOURS_"
+    EVERY_2_HOURS = "_2_HOURS_"
+    # 4 hours
+    EVERY_4_HOUR = "_4_HOURS_"
+    EVERY_4_HOURS = "_4_HOURS_"
+    # 8 hours
+    EVERY_8_HOUR = "_8_HOURS_"
+    EVERY_8_HOURS = "_8_HOURS_"
+    # 12 hours
+    EVERY_12_HOUR = "_12_HOURS_"
+    EVERY_12_HOURS = "_12_HOURS_"
+    # 1 day
+    DAILY = "_1_DAY_"
+    EVERY_DAY = "_1_DAY_"
+    EVERY_1_DAY = "_1_DAY_"
+    EVERY_1_DAYS = "_1_DAY_"
+    # 2 days
+    EVERY_2_DAY = "_2_DAYS_"
+    EVERY_2_DAYS = "_2_DAYS_"
+    # Amazon chose to do 72 hours instead of "3 days" (see below),
+    # so I'm adding a 48 for consistency.
+    EVERY_48_HOUR = "_2_DAYS_"
+    EVERY_48_HOURS = "_2_DAYS_"
+    # 3 days
+    EVERY_3_DAY = "_72_HOURS_"  # Don't ask me, it's their API.
+    EVERY_3_DAYS = "_72_HOURS_"
+    EVERY_72_HOUR = "_72_HOURS_"
+    EVERY_72_HOURS = "_72_HOURS_"
+    # 1 week
+    WEEKLY = "_1_WEEK_"
+    EVERY_WEEK = "_1_WEEK_"
+    EVERY_1_WEEK = "_1_WEEK_"
+    EVERY_1_WEEKS = "_1_WEEK_"
+    EVERY_7_DAY = "_1_WEEK_"
+    EVERY_7_DAYS = "_1_WEEK_"
+    # 2 weeks
+    EVERY_14_DAY = "_14_DAYS_"  # Again with the inconsistency
+    EVERY_14_DAYS = "_14_DAYS_"
+    EVERY_2_WEEK = "_14_DAYS_"
+    EVERY_2_WEEKS = "_14_DAYS_"
+    FORTNIGHTLY = "_14_DAYS_"  # Makes sense for some
+    # 15 days
+    # Technically 'semi-monthly' makes sense here, too,
+    # but the definition of that is not 15 days. So, skip it.
+    EVERY_15_DAY = "_15_DAYS_"
+    EVERY_15_DAYS = "_15_DAYS_"
+    # 30 days
+    EVERY_30_DAY = "_30_DAYS_"
+    EVERY_30_DAYS = "_30_DAYS_"
+    # Delete a previously created report request schedule
+    DELETE = "_NEVER_"
+
+
+# Attach the Enums to the Reports API for convenience.
+Reports.ReportType = ReportType
+Reports.Schedule = Schedule
