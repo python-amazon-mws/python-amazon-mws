250a922bf2432765c1d91974144866b6d4c72196
Galen Rice <galen.s.rice@gmail.com>
2021-01-27 07:39:29 +0000

    Adjust sphinx to autodoc members in source order

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,9 @@ extensions = [
     "sphinx.ext.autodoc",
 ]
 
+# Default "alphabetical", change to source ordering:
+autodoc_member_order = "bysource"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
