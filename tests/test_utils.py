688e3549235d607b32546eaf611d93a4360fdede
Michael Manganiello <adamantike@users.noreply.github.com>
2021-06-04 20:01:25 -0300

    Fix double encoding of string parameters (#263)
    Currently, the `0.8.x` branch breaks string parameters by encoding them
    twice, first in the `clean_params_dict` function, and then in
    `calc_request_description`. Because of this, parameters prone to have
    characters that must be escaped, like `NextToken` and URLs, do not work
    correctly.
    
    https://github.com/python-amazon-mws/python-amazon-mws/pull/258 was an
    attempt to fix this issue, but only for timestamp fields. Strings are
    still broken.
    
    This solution is a backport of a commit
    (https://github.com/python-amazon-mws/python-amazon-mws/pull/65) that is already present in the
    `develop` branch, where the `calc_request_description` function is no
    longer responsible for escaping the received parameters.
    
    It also rollbacks
    https://github.com/python-amazon-mws/python-amazon-mws/pull/258,
    as `clean_date` must encode the received values now.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from mws.mws import calc_md5, calc_request_description
+from mws.mws import calc_md5, calc_request_description, clean_params_dict
 
 
 def test_calc_md5():
@@ -11,7 +11,7 @@ def test_calc_request_description(access_key, account_id):
             "AWSAccessKeyId": access_key,
             "Markets": account_id,
             "SignatureVersion": "2",
-            "Timestamp": "2017-08-12T19:40:35Z",
+            "Timestamp": "2017-08-12T19%3A40%3A35Z",
             "Version": "2017-01-01",
             "SignatureMethod": "HmacSHA256",
         }
@@ -25,6 +25,37 @@ def test_calc_request_description(access_key, account_id):
         + account_id
         + "&SignatureMethod=HmacSHA256"
         "&SignatureVersion=2"
+        "&Timestamp=2017-08-12T19%3A40%3A35Z"
+        "&Version=2017-01-01"
+    )
+
+
+def test_calc_request_description_for_cleaned_params(access_key, account_id):
+    params = clean_params_dict(
+        {
+            "AWSAccessKeyId": access_key,
+            "Markets": account_id,
+            "Subscription.Destination.AttributeList.member.1.Key": "sqsQueueUrl",
+            "Subscription.Destination.AttributeList.member.1.Value": "https://sqs.us-east-1.amazonaws.com/123456789/mws-notifications",
+            "SignatureVersion": "2",
+            "Timestamp": "2017-08-12T19:40:35Z",
+            "Version": "2017-01-01",
+            "SignatureMethod": "HmacSHA256",
+        }
+    )
+    request_description = calc_request_description(params)
+
+    assert not request_description.startswith("&")
+    assert request_description == (
+        "AWSAccessKeyId="
+        + access_key
+        + "&Markets="
+        + account_id
+        + "&SignatureMethod=HmacSHA256"
+        "&SignatureVersion=2"
+        "&Subscription.Destination.AttributeList.member.1.Key=sqsQueueUrl"
+        "&Subscription.Destination.AttributeList.member.1.Value="
+        "https%3A%2F%2Fsqs.us-east-1.amazonaws.com%2F123456789%2Fmws-notifications"
         "&Timestamp=2017-08-12T19%3A40%3A35Z"
         "&Version=2017-01-01"
     )
